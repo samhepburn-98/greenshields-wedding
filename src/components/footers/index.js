08127d80ff78b7b499d388ed0c062f28cb5dcd2f
samhepburn-98 <samhepburn98@msn.com>
2023-01-04 14:15:48 +0000

    Comment out redundant links

--- src/components/footers/index.js
+++ src/components/footers/index.js
@@ -1,16 +1,6 @@
 import React from "react";
 import { ReactComponent as FacebookIcon } from "../../images/facebook-icon.svg";
-import {
-    Container,
-    Content,
-    CopyrightText,
-    Link,
-    LinksContainer,
-    LogoText,
-    Row,
-    SocialLink,
-    SocialLinksContainer
-} from "./styles";
+import { Container, Content, CopyrightText, LogoText, Row, SocialLink, SocialLinksContainer } from "./styles";
 
 export default () => {
     return (
@@ -18,13 +8,13 @@ export default () => {
             <Content>
                 <Row>
                     <LogoText>M&M</LogoText>
-                    <LinksContainer>
-                        <Link href="#">Home</Link>
-                        <Link href="#">About</Link>
-                        <Link href="#">Contact Us</Link>
-                        <Link href="#">Blog</Link>
-                        <Link href="#">Reviews</Link>
-                    </LinksContainer>
+                    {/*<LinksContainer>*/}
+                    {/*    <Link href="#">Home</Link>*/}
+                    {/*    <Link href="#">About</Link>*/}
+                    {/*    <Link href="#">Contact Us</Link>*/}
+                    {/*    <Link href="#">Blog</Link>*/}
+                    {/*    <Link href="#">Reviews</Link>*/}
+                    {/*</LinksContainer>*/}
                     <SocialLinksContainer>
                         <SocialLink href="https://facebook.com">
                             <FacebookIcon/>

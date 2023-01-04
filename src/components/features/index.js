bd4df8f39a8e7c2fa10ae220f06e1103201094d0
samhepburn-98 <samhepburn98@msn.com>
2023-01-04 11:06:45 +0000

    Update features and add location details and images

--- src/components/features/index.js
+++ src/components/features/index.js
@@ -1,13 +1,10 @@
 import React from "react";
-import { SectionHeading as HeadingTitle } from "../misc/Headings.js";
 import {
     Card,
     Container,
     Content,
     Description,
     Details,
-    HeadingDescription,
-    HeadingInfoContainer,
     Image,
     Link,
     SingleColumn,
@@ -18,39 +15,39 @@ import {
     SvgDotPattern4,
     Title
 } from "./styles";
+import churchPic from "../../images/ChurchOldPic.jpeg"
+import venuePic from "../../images/huge.jpg"
 
 export default () => {
     const cards = [
         {
-            imageSrc:
-                "https://images.unsplash.com/photo-1550699026-4114bbf4fb49?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80",
-            subtitle: "Paid",
-            title: "Loachella, NYC",
+            imageSrc: churchPic,
+            subtitle: "12:00",
+            title: "The Ceremony",
             description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            url: "https://timerse.com"
+                "Our Lady & St Joseph’s Church\nBank Road\nMatlock\nDE4 3NG",
+            url: "https://goo.gl/maps/B6sFaJnn9SwHbtF7A"
+        },
+        {
+            imageSrc: venuePic,
+            subtitle: "18:00",
+            title: "The Reception",
+            description:
+                "The White Hart Inn\nInns Lane\nMoorwood Moor\nAlfreton\nDE55 7NU",
+            url: "https://goo.gl/maps/zTJ6khM2gq9Gvnkt8"
         },
 
-        {
-            imageSrc:
-                "https://images.unsplash.com/photo-1543423924-b9f161af87e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=634&q=80",
-            subtitle: "Free",
-            title: "Rock In Rio, Upstate",
-            description:
-                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-            url: "https://timerse.com"
-        },
     ];
 
     return (
         <Container>
             <SingleColumn>
-                <HeadingInfoContainer>
-                    <HeadingTitle>Popular Events</HeadingTitle>
-                    <HeadingDescription>
-                        Here are some of the most popular events in New York City curated by professionals.
-                    </HeadingDescription>
-                </HeadingInfoContainer>
+                {/*<HeadingInfoContainer>*/}
+                {/*<HeadingTitle>Popular Events</HeadingTitle>*/}
+                {/*<HeadingDescription>*/}
+                {/*    Here are some of the most popular events in New York City curated by professionals.*/}
+                {/*</HeadingDescription>*/}
+                {/*</HeadingInfoContainer>*/}
 
                 <Content>
                     {
@@ -60,8 +57,8 @@ export default () => {
                                 <Details>
                                     <Subtitle>{card.subtitle}</Subtitle>
                                     <Title>{card.title}</Title>
-                                    <Description>{card.description}</Description>
-                                    <Link href={card.url}>See Event Details</Link>
+                                    <Description style={{ whiteSpace: "pre-wrap" }}>{card.description}</Description>
+                                    <Link href={card.url}>Tap for directions</Link>
                                 </Details>
                             </Card>
                         ))
--- src/components/features/styles.js
+++ src/components/features/styles.js
@@ -4,15 +4,15 @@ import { ReactComponent as SvgDotPatternIcon } from "../../images/dot-pattern.sv
 
 export const Container = tw.div`relative`;
 
-export const SingleColumn = tw.div`max-w-screen-xl mx-auto py-20 lg:py-24`;
+export const SingleColumn = tw.div`max-w-screen-xl mx-auto py-10 lg:py-12`;
 
 export const HeadingInfoContainer = tw.div`flex flex-col items-center`;
 export const HeadingDescription = tw.p`mt-4 font-medium text-gray-600 text-center max-w-sm`;
 
-export const Content = tw.div`mt-16`;
+export const Content = tw.div`mt-8`;
 
 export const Card = styled.div(props => [
-    tw`mt-24 md:flex justify-center items-center`,
+    tw`mt-12 md:flex justify-center items-center`,
     props.reversed ? tw`flex-row-reverse` : "flex-row"
 ]);
 export const Image = styled.div(props => [
@@ -20,9 +20,9 @@ export const Image = styled.div(props => [
     tw`rounded md:w-1/2 lg:w-5/12 xl:w-1/3 flex-shrink-0 h-80 md:h-144 bg-cover bg-center mx-4 sm:mx-8 md:mx-4 lg:mx-8`
 ]);
 export const Details = tw.div`mt-4 md:mt-0 md:max-w-md mx-4 sm:mx-8 md:mx-4 lg:mx-8`;
-export const Subtitle = tw.div`font-bold tracking-wide text-secondary-100`;
+export const Subtitle = tw.div`font-bold tracking-wide text-primary-500`;
 export const Title = tw.h4`text-3xl font-bold text-gray-900`;
-export const Description = tw.p`mt-2 text-sm leading-loose`;
+export const Description = tw.p`mt-2 text-sm leading-loose text-black`;
 export const Link = tw.a`inline-block mt-4 text-sm text-primary-500 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-primary-500`;
 
 export const SvgDotPattern1 = tw(

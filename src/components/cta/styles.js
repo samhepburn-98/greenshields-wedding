2b9b750f34ff88df7c45bd50ec2e0dd53664694f
samhepburn-98 <samhepburn98@msn.com>
2023-03-10 15:44:38 +0000

    Rsvp button text color navy

--- src/components/cta/styles.js
+++ src/components/cta/styles.js
@@ -15,7 +15,7 @@ export const Subheading = tw(SubheadingBase)`text-white mb-2 tracking-wider`
 
 export const LinksContainer = tw.div`mt-4 lg:mt-8 flex flex-col items-center sm:block`
 export const Link = styled.a`
-  ${tw`w-56 p-3 sm:p-4 text-sm sm:text-base font-bold uppercase tracking-wider rounded-full inline-flex justify-center items-center mt-6 first:mt-0 sm:mt-0 sm:ml-8 first:ml-0 bg-secondary-500 hocus:bg-secondary-700 text-white hocus:text-white shadow hover:shadow-lg focus:shadow-outline focus:outline-none transition duration-300`}
+  ${tw`w-56 p-3 sm:p-4 text-sm sm:text-base font-bold uppercase tracking-wider rounded-full inline-flex justify-center items-center mt-6 first:mt-0 sm:mt-0 sm:ml-8 first:ml-0 bg-secondary-500 hocus:bg-secondary-700 text-primary-500 hocus:text-primary-500 shadow hover:shadow-lg focus:shadow-outline focus:outline-none transition duration-300`}
   img {
     ${tw`inline-block h-8 mr-3`}
   }

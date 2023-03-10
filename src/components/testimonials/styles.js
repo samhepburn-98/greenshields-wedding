7bfcd349067d605a441662e43e0d8bc255936d3c
samhepburn-98 <samhepburn98@msn.com>
2023-03-10 16:04:15 +0000

    Navy taxi services text

--- src/components/testimonials/styles.js
+++ src/components/testimonials/styles.js
@@ -9,8 +9,8 @@ export const Testimonials = tw.div`flex flex-col lg:flex-row items-center lg:ite
 export const TestimonialContainer = tw.div`mt-16 lg:w-1/3`;
 export const Testimonial = tw.div`px-4 text-center max-w-xs mx-auto flex flex-col items-center`;
 export const Image = tw.img`w-20 h-20 rounded-full drop-shadow object-contain`;
-export const Quote = tw.blockquote`mt-5 text-gray-600 font-medium text-xl leading-loose`;
-export const Link = tw.a`inline-block text-base text-secondary-600 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent hover:border-secondary-500`;
+export const Quote = tw.blockquote`mt-5 text-primary-500 font-medium text-xl leading-loose`;
+export const Link = tw.a`inline-block text-base text-primary-400 font-bold cursor-pointer transition duration-300 border-b-2 border-transparent border-primary-400`;
 
 export const DecoratorBlob1 = styled(SvgDecoratorBlob1)`
   ${tw`pointer-events-none -z-20 absolute left-0 top-0 h-56 w-56 opacity-15 transform -translate-x-2/3 -translate-y-12 text-secondary-400`}
